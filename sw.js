809b074c14e40bf9c1eac69a8db6477e3a57d724
Raul Gonzalez <raul.gonzalez@blinn.edu>
2025-08-28 02:50:47 -0500

    updates

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@ self.addEventListener('push', function(event) {
     var title = 'Registration is now open';
     var body = 'We have received a push message.';
     var icon = 'https://www.blinn.edu/_files/images/ico/favicon.ico';
-    var tag = 'tag-23';
+    var tag = 'tag-24';
     var data = {
     doge: {
         wow: 'such amaze notification data'
